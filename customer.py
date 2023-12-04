d45580096697d897a0a55da2fc3eb589598e2839
userom11 <126608941+userom11@users.noreply.github.com>
2023-12-04 18:59:49 +0200

    Update customer.py

--- customer.py
+++ customer.py
@@ -4,6 +4,11 @@
 
 import socket
 
+# The ip of the server we want to send what we want to order to
+server_ip=('127.0.0.1')
+# The port we using
+port=('37373')
+
 # ipv4, tcp socket object
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # connecting to the server
